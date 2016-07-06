3c46c3ebb0144758fb367f5c36f1b3a76c0f8ad0
smukkecharlie <canna.bissen@rocketmail.com>
2016-07-06 09:52:04 +0200

    Updated the fswatch test program

--- c/fswatch_test.c
+++ c/fswatch_test.c
@@ -3,6 +3,18 @@
 #include <libfswatch/c/libfswatch.h>
 
 /**
+ * To compile this program you'll have to link against libfswatch to sort any issues with dunamic references
+ * Remember to use a dynamic library i.e.
+ * $ gcc -I /usr/local/include -o "fswatch_test" fswatch_test.c /usr/local/lib/libfswatch.dylib 
+ * 
+ * You run the program like this:
+ * $ ./fswatch_test ../bash/
+ *
+ * The only output is callback for now, but you can update it as you please, its meant for testing the c compilation and linkage
+ * in a c enviroment.
+ **/
+
+/**
  * The following function implements the callback functionality for testing eventnumber send from the
  * libdawatch library. See FSW_CEVENT_CALLBACK for details.
  */
@@ -12,31 +24,19 @@ void my_callback(fsw_cevent const * const events, const unsigned int event_num, 
 
 int main(int argc, char **argv) {
 
-  //  if(2 <= argc) {
+  if(2 <= argc) {
     if(FSW_OK == fsw_init_library()) {
 
-      //      FSW_HANDLE fsw_init_session(const fsw_monitor_type type = system_default_monitor_type);
-      /* const FSW_HANDLE handle = fsw_init_session(/\*system_default_monitor_type*\/fsevents_monitor_type); */
-      /* if(FSW_INVALID_HANDLE != handle) { */
-
-      /* 	/\* Loop and all all paths passed to the program *\/ */
-      /* 	int j = 0; */
-      /* 	for(j=1;j<argc;j++) { */
-      /* 	  if(FSW_OK != fsw_add_path(handle,argv[j])) { */
-      /* 	    fsw_last_error(); */
-      /* 	  } */
-      /* 	} */
-
-      const FSW_HANDLE handle = fsw_init_session(/*system_default_monitor_type*/fsevents_monitor_type);
+      const FSW_HANDLE handle = fsw_init_session(fsevents_monitor_type);
       if(FSW_INVALID_HANDLE != handle) {
 
 	/* Loop and all all paths passed to the program */
-	//	int j = 0;
-	//for(j=1;j<argc;j++) {
-	  if(FSW_OK != fsw_add_path(handle,"/Users/cannabissen/Documents/src/bash")) {
+	int j = 0;
+	for(j=1;j<argc;j++) {
+	  if(FSW_OK != fsw_add_path(handle,argv[j])) {
 	    fsw_last_error();
 	  }
-	  //}
+	}
 
 	if(FSW_OK != fsw_set_callback(handle, my_callback,NULL)) {
 	  fsw_last_error();
@@ -57,9 +57,9 @@ int main(int argc, char **argv) {
       fsw_last_error();	  
       (void)printf("fswatch cannot be initialised!\n");
     }
-    //  }
-//  else {
-//    (void)printf("usage: %s [path]\n",argv[0]);
-//  }
+  }
+  else {
+    (void)printf("usage: %s [path]\n",argv[0]);
+  }
   return 0;
 }
